0bc5eaea23d28e3adbf3d81370526f57cda6aea9
sheiderxd <sheiderxd@gmail.com>
2024-02-09 12:17:51 +0200

    user password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "root",
+      password: "root",
+      database: "tgtaxi",
+      entities: [],
+      synchronize: false,
+    }),      
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+const crypto = require("crypto");
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar" })
+  name: string;
+
+  @Column({ type: "varchar" })
+  username: string;
+
+  @Column({ type: "varchar" })
+  email: string;
+
+  @Column({ type: "varchar" })
+  resetPasswordToken: {
+    type: string;
+  };
+
+  @Column({ type: "varchar" })
+  encryptionHash: string;
+
+  @Column({ type: "varchar" })
+  encryptedPassword: string;
+
+  setPassword(password: string) {
+    const encryptionHash = crypto.randomBytes(128).toString("base64");
+    this.encryptionHash = encryptionHash;
+    this.encryptedPassword = encrypt(password, encryptionHash);
+  }
+
+  verifyPassword(password: string): boolean {
+    return encrypt(password, this.encryptionHash) === this.encryptedPassword;
+  }
+}
+
+function encrypt(password: string, encryptionHash: string): string {
+  return crypto
+    .pbkdf2Sync(password, encryptionHash, 1, 128, "sha1")
+    .toString("base64");
+}
